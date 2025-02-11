1706a7900ff224ee5f874da24336cdf630cba0fb
nezbut <anonim@anonim.com>
2025-02-12 05:17:42 +0700

    feat: update files

--- handlers/user.go
+++ handlers/user.go
@@ -1,7 +1,15 @@
 package handlers
 
-import "github.com/gofiber/fiber/v3"
+import (
+	"fmt"
 
-func MainRoute(c fiber.Ctx) error {
-	return c.SendString("Hello Nezbut from HTTP World")
+	"github.com/fatih/color"
+	"github.com/gofiber/fiber/v2"
+)
+
+func MainRoute(c *fiber.Ctx) error {
+	var col = color.New(color.BgCyan).Add(color.BgRed)
+	str := fmt.Sprintf("Hello Nezbut from HTTP World, ip: %s", c.IP())
+	col.Println(str)
+	return c.SendString(str)
 }
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"log"
 
-	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v2"
 	"github.com/nezbut/hello-http-go/handlers"
 )
 
